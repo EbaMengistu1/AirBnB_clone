4a3d3a529be92796f7de2cc83302a07fd692a497
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-01-26 22:32:30 +0100

    [Fix]: adding test for BaseModel instances

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -10,16 +10,32 @@ from models.base_model import BaseModel
 
 
 class TestAmenity(unittest.TestCase):
-    """
-    Tests for amenities
-    """
+    """Tests instances and methods from amenity class"""
 
-    def test_name(self):
-        """
-        Tests for name inputs
-        """
-        pass
+    a = Amenity()
 
+    def test_class_exists(self):
+        """tests if class exists"""
+        res = "<class 'models.amenity.Amenity'>"
+        self.assertEqual(str(type(self.a)), res)
+
+    def test_user_inheritance(self):
+        """test if Amenity is a subclass of BaseModel"""
+        self.assertIsInstance(self.a, Amenity)
+
+    def testHasAttributes(self):
+        """verify if attributes exist"""
+        self.assertTrue(hasattr(self.a, 'name'))
+        self.assertTrue(hasattr(self.a, 'id'))
+        self.assertTrue(hasattr(self.a, 'created_at'))
+        self.assertTrue(hasattr(self.a, 'updated_at'))
+
+    def test_types(self):
+        """tests if the type of the attribute is the correct one"""
+        self.assertIsInstance(self.a.name, str)
+        self.assertIsInstance(self.a.id, str)
+        self.assertIsInstance(self.a.created_at, datetime.datetime)
+        self.assertIsInstance(self.a.updated_at, datetime.datetime)
 
 if __name__ == '__main__':
     unittest.main()
